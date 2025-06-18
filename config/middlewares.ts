ec82d77690440713f5fa9622b7d48a20148bfed5
Yokub <yoqub.madixanov@outlook.com>
2025-06-18 16:11:55 +0500

    Giving all permissions to Telegram

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,14 +1,42 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'cdn.jsdelivr.net', 'strapi.io', 't.me'],
+          'media-src': ["'self'", 'data:', 'blob:', 'cdn.jsdelivr.net', 'strapi.io', 't.me'],
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'frame-src': ["'self'", 't.me', 'telegram.org'],
+        },
+      },
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
-      origin: ['https://mbclicker.netlify.app/'],
-      methods: ['GET', 'POST', 'PUT'],
-      headers: '*',
+      origin: [
+        'https://mbclicker.netlify.app',
+        'https://web.telegram.org',
+        'https://telegram.org',
+        /\.telegram\.org$/,
+        /\.t\.me$/,
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'Origin',
+        'Accept',
+        'X-Requested-With',
+        'X-Telegram-Init-Data',
+      ],
       credentials: true,
+      keepHeaderOnError: true,
     },
   },
   'strapi::poweredBy',
