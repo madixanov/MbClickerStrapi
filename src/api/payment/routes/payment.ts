033b20dc1e72772127ab32873513bf51586b51da
Yokub <yoqub.madixanov@outlook.com>
2025-07-12 17:49:59 +0500

    Creating payment content-type

--- src/api/payment/routes/payment.ts
+++ src/api/payment/routes/payment.ts
@@ -1,36 +1,16 @@
-export default {
-  routes: [
-    {
-      method: 'POST',
-      path: '/payment/create',
-      handler: 'payment.create',
-      config: {
-        auth: false,
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::payment.payment' as any, {
+  config: {
+    routes: [
+      {
+        method: 'POST',
+        path: '/payment/create',
+        handler: 'payment.create',
+        config: {
+          auth: false,
+        },
       },
-    },
-    {
-      method: 'GET',
-      path: '/payments',
-      handler: 'payment.find',
-      config: {
-        auth: false, // или true, если хочешь защитить
-      },
-    },
-    {
-      method: 'GET',
-      path: '/payments/:documentId',
-      handler: 'payment.findOne',
-      config: {
-        auth: false,
-      },
-    },
-    {
-      method: 'PUT',
-      path: '/payments/:documentId',
-      handler: 'payment.update',
-      config: {
-        auth: false,
-      },
-    },
-  ],
-};
+    ],
+  },
+});
